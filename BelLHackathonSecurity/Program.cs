aa9691e39b2fe2536557ebfba07c1b7079787f94
Phi Khanh Hoang <94206447+PhiKhanhHoang@users.noreply.github.com>
2023-09-23 15:31:19 -0400

    changed program.cs

--- BelLHackathonSecurity/Program.cs
+++ BelLHackathonSecurity/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddScoped<IDataManager, DataManager>();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
