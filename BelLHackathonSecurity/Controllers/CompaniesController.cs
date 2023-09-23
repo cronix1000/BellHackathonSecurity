7078f743f746bed58cadb24241e06a9c9dc53248
Phi Khanh Hoang <94206447+PhiKhanhHoang@users.noreply.github.com>
2023-09-23 15:10:12 -0400

    added create company page

---
+++ BelLHackathonSecurity/Controllers/CompaniesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BelLHackathonSecurity.Data;
+using BelLHackathonSecurity.Models;
+
+namespace BelLHackathonSecurity.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CompaniesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        // GET: Companies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Companies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CompanyName,CompanyLink,CompanyImage")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                company.Id = Guid.NewGuid();
+                _context.Add(company);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
+        }
+
+     
+
+
+
+
+    }
+}
